f7ae2b7a1720d0e528d6381e09a3dc11c8fe310c
Crypt06545 <mehedihasansagor2005@gmail.com>
2024-12-05 10:36:50 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const dotenv = require("dotenv");
 const express = require("express");
-const app = express();
-const port = 3000;
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+dotenv.config(); // Load environment variables
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@bot.jxczvgl.mongodb.net/?retryWrites=true&w=majority&appName=bot`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    // create a database
+    const database = client.db("VisaDB");
+    const usersCollection = database.collection("NewVisa");
+
+    // get
+
+    app.get("/", (req, res) => {
+      res.send("hello");
+    });
+    // post
+    app.post("/addvisa", async (req, res) => {
+      const newVisa = req.body;
+      console.log(newVisa);
+    });
+
+    // delete
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
