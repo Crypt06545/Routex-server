6d065acb3f18b827dee796fb85217a18dc8f795e
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-05-15 14:01:38 +0600

    deployment

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     //create a database
     const database = client.db("VisaDB");
@@ -128,10 +128,10 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
